c78d1d02c3f1e8a2469cb0a4f2c248ebcee73b65
nicolasandre-dev <nicolas.andre@escola.pr.gov.br>
2026-05-13 11:00:19 -0300

    feito

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-// Criando o array (Referência: Slide 9)
 let frutas = ['Maçã', 'Banana', 'Laranja', 'Morango', 'Uva'];
-
-// Mostra a lista completa (Referência: Slide 13)
 console.log(frutas);
-
-// Mostra a primeira fruta (Índice 0 - Referência: Slide 10)
 console.log(frutas[0]);
-
-// Mostra a última fruta (Usando length - Referência: Slide 10)
 console.log(frutas[frutas.length - 1]);
 
-let numeros = [10, 20, 30, 40, 50, 60];
+let numeros = [104, 200, 479, 40, 50, 400];
 let soma = 0;
 
-// Percorrendo a lista (Erro 'inde' removido aqui)
 for (let i = 0; i < numeros.length; i++) {
     soma += numeros[i];
 }
-
 console.log("A soma total é: " + soma);
 
-let listaNumeros = [5, 12, 8, 25, 3, 45, 1, 18, 22, 10];
+let listaNumeros = [5, 12, 739, 25, 3, 45, 1, 500, 22, 10];
 let maior = listaNumeros[0];
 
 for (let i = 1; i < listaNumeros.length; i++) {
@@ -28,14 +19,14 @@ for (let i = 1; i < listaNumeros.length; i++) {
         maior = listaNumeros[i];
     }
 }
-
 console.log("O maior valor encontrado é: " + maior);
 
 let aluno = ["Carlos Silva", 17, "3º Ano A"];
-console.log("Nome: " + aluno[0] + ", Idade: " + aluno[1] + ", Turma: " + aluno[2]);
+console.log(`Nome: ${aluno[0]} | Idade: ${aluno[1]} | Turma: ${aluno[2]}`);
 
 let produto = ["Caderno", 15.90, 50];
-console.log("O produto " + produto[0] + " custa R$" + produto[1] + " e temos " + produto[2] + " em estoque.");
+// .toFixed(2) força o número a mostrar duas casas decimais (R$15.90 em vez de R$15.9)
+console.log(`O produto ${produto[0]} custa R$${produto[1].toFixed(2)} e temos ${produto[2]} em estoque.`);
 
 let filmes = [
     ["Interestelar", 2014, "Ficção"],
@@ -43,7 +34,6 @@ let filmes = [
     ["Matrix", 1999, "Ação"]
 ];
 
-// Percorrendo e mostrando os dados
 for (let i = 0; i < filmes.length; i++) {
-    console.log("Filme: " + filmes[i][0] + " | Ano: " + filmes[i][1] + " | Gênero: " + filmes[i][2]);
+    console.log(`Filme: ${filmes[i][0]} | Ano: ${filmes[i][1]} | Gênero: ${filmes[i][2]}`);
 }
